9668bcaa721e1658ffb441c0fdfb7632c381f463
JdyxZ <javier.serrano04@estudiant.upf.edu>
2023-04-16 20:33:36 +0200

    Library update

--- PCA.R
+++ PCA.R
@@ -1,37 +1,38 @@
-#Precàrrega de llibreries i paquets necessaris pel processat de dades i l'estudi PCA
+#Prec?rrega de llibreries i paquets necessaris pel processat de dades i l'estudi PCA
 install.packages("factoextra")
 install.packages("FactoMineR")
+install.packages("readxl")
+install.packages("rgl")
 library(factoextra)
 library(FactoMineR)
-
-#Càrrega del dataset de les dades del pis de referència - Parellada (A)
 library(stats)
-install.packages("readxl")
 library("readxl")
+library(car)
+library(carData)
+library(rgl)
+library("rgl")
+library("nlme")
+library("mgcv")
+library(readxl)
+
+#C?rrega del dataset de les dades del pis de refer?ncia - Parellada (A)
 Parellada <- read_excel("CASA_PARELLADAS.xlsx")
 datos_nci <- Parellada[2:19]
 
 #Escalat de dades de la matriu per a plotejar correctament el conjunt
 scaled_data <- scale(datos_nci)
 
-#Generació del model PCA en 2D i plotejat de gràfics representatius
+#Generaci? del model PCA en 2D i plotejat de gr?fics representatius
 pca2d <- PCA(datos_nci,ncp=5,graph=FALSE)
 fviz_pca_var(pca, col.var = "contrib") # Scree plot
 fviz_pca_biplot(pca, col.var = "contrib") # Biplot
 
-#Generació del model PCA per plotejat 3D
-library(car)
-library(carData)
-install.packages("rgl")
-library(rgl)
-library("rgl")
-library("nlme")
-library("mgcv")
+#Generaci? del model PCA per plotejat 3D
 scaled_data <- scale(datos_nci)
 rotated_data <- prcomp(scaled_data, center=TRUE, scale.=TRUE)$x
 pca3d <- PCA(rotated_data,ncp=3,graph=FALSE)
 
-#Dades i grafics interessants sobre la variància i desviació estàndard de les diferents variables
+#Dades i grafics interessants sobre la vari?ncia i desviaci? est?ndard de les diferents variables
 summary(pca3d)
 var_prop <- pca3d$sdev^2/sum(pca3d$sdev^2)
 
@@ -43,7 +44,6 @@ cor_matrix <- cor(datos_nci)
 plot3d(cor_matrix, type = "s", size = 2)
 
 #Carreguem les dades dels altres pisos
-library(readxl)
 Pompeu<- read_excel(path = "CASA_PADRE_MARC.xlsx")
 datos_Pompeu<- Pompeu[,2:19]
 scaled_Pompeu <- scale(datos_Pompeu)
@@ -58,7 +58,7 @@ scatter3d(pca3d$ind$coord[,1], pca3d$ind$coord[,2], pca3d$ind$coord[,3],color = 
 
 points3d(x, y, z, col = "red", pch = 19, cex = 1.5, xlab = "Eje X", zlab = "Eje Z")
 
-#Plotejat de l'el·lipse al voltant del set de dades principal
+#Plotejat de l'el?lipse al voltant del set de dades principal
 
 x <- pca$ind$coord[, 1]
 y <- pca$ind$coord[, 2]
@@ -71,7 +71,7 @@ z1<-pcm$center[3]
 
 datos<-data.frame(x,y,z,x1,y1,z1)
 
-# Ajustar l'el·lipse al voltant del conjunt de dades principals
+# Ajustar l'el?lipse al voltant del conjunt de dades principals
 cov_matrix <- cov(datos[, c("x", "y", "z")])
 eigen_values <- eigen(cov_matrix)$values
 eigen_vectors <- eigen(cov_matrix)$vectors
@@ -82,7 +82,7 @@ radius_z <- sqrt(qchisq(0.95, df = 3) * eigen_values[3])
 orientation <- eigen_vectors
 position <- center - c(radius_x, radius_y, radius_z) %*% t(orientation)
 
-# Plotejar l'el·lipse alvoltant del conjunt de dades principals
+# Plotejar l'el?lipse alvoltant del conjunt de dades principals
 open3d()
 
 # Verificar que x sigui una matriu
@@ -90,7 +90,7 @@ if (!is.matrix(x)) {
   stop("Error: x is not a matrix.")
 } else {
   
-  # Continuar con el resto del código
+  # Continuar con el resto del c?digo
 }
 rgl::ellipse3d(center = center, radii = c(radius_x, radius_y, radius_z), 
                orientation = orientation, position = position, color = "blue")
