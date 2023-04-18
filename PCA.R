27b4300b0364ca0dd0bc43dc31537dc1c608377b
JdyxZ <javier.serrano04@estudiant.upf.edu>
2023-04-18 12:02:38 +0200

    Update

--- PCA.R
+++ PCA.R
@@ -46,10 +46,10 @@ fviz_contrib(pca3d, choice = "var", axes = 1, top = 10)
 
 #Plotejat 3D del model PCA
 scatter3d(pca3d$ind$coord[,1], pca3d$ind$coord[,2], pca3d$ind$coord[,3], point.col = "steelblue",
-          pch = 16, cex = 1000, xlab = "PC1", ylab = "PC2", zlab = "PC3", surface = FALSE, ellipsoid = TRUE)
+          sphere.size = 1.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", surface = FALSE, ellipsoid = TRUE)
 
-#plot3d(pca3d$ind$coord[,1], pca3d$ind$coord[,2], pca3d$ind$coord[,3], col = "blue", 
-        #type = "s", size = 0.5, xlab = "PC1", ylab = "PC2", zlab = "PC3")
+# plot3d(pca3d$ind$coord[,1], pca3d$ind$coord[,2], pca3d$ind$coord[,3], col = "steelblue", 
+        # type = "s", size = 0.5, xlab = "PC1", ylab = "PC2", zlab = "PC3")
 
 cor_matrix <- cor(datos_nci)
 plot3d(cor_matrix, type = "s", size = 2)
@@ -60,14 +60,26 @@ datos_Pompeu<- Pompeu[,2:19]
 scaled_Pompeu <- scale(datos_Pompeu)
 rotated_Pompeu <- prcomp(scaled_Pompeu, center=TRUE, scale.=TRUE)$x
 Pompeu_coords <- predict(pca3d, rotated_Pompeu)
-x <- Pompeu_coords$coord[, 1]
-y <- Pompeu_coords$coord[, 2]
-z <- Pompeu_coords$coord[, 3]
 
-scatter3d(pca3d$ind$coord[,1], pca3d$ind$coord[,2], pca3d$ind$coord[,3],point.col = "blue",
-          pch = 19, cex = 1.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", surface=FALSE,ellipsoid = TRUE)
+# Get values lengths
+PCA_Length <- nrow(pca3d$ind$coord)
+Predicted_Length <- nrow(Pompeu_coords$coord)
 
-"points3d(x, y, z, col = "red", pch = 19, cex = 1.5, xlab = "Eje X", zlab = "Eje Z")"
+# Combine PCA analysis values and new predicted values of the model into an array
+x <- c(pca3d$ind$coord[,1], Pompeu_coords$coord[, 1])
+y <- c(pca3d$ind$coord[,2], Pompeu_coords$coord[, 2])
+z <- c(pca3d$ind$coord[,3], Pompeu_coords$coord[, 3])
 
-scatter3d(x, y, z, point.col = "green", pch = 19, cex = 1.5, xlab = "Eje X", zlab = "Eje Z", surface=FALSE)
+# Set a color for each type of values
+colors <- c(rep("blue", PCA_Length), rep("green", Predicted_Length))
+
+# Create a grouping variable
+groups <- factor(c(rep("PCA Analysis", PCA_Length), rep("Predicted", Predicted_Length)))
+
+# Plot values
+scatter3d(x, y, z, groups = groups, point.col = colors, sphere.size = 1.7,
+          xlab = "PC1", ylab = "PC2", zlab = "PC3", surface=FALSE)
+
+# Add legend
+legend3d("topright", legend = c("PCA Analysis", "Predicted"), col = c("blue", "green"), pch = 16)
 
