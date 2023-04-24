8779de9a3543166fe3d42dfe23a753bc47b074df
JdyxZ <javier.serrano04@estudiant.upf.edu>
2023-04-24 16:56:32 +0200

    Guardaropas

--- PCA.R
+++ PCA.R
@@ -7,6 +7,7 @@ install.packages("rgl")
 install.packages("mgcv")
 install.packages("nlme")
 install.packages("car", dependencies = TRUE)
+install.packages("scatterplot3d")
 
 # Load libraries into memory 
 library(factoextra)
@@ -18,61 +19,72 @@ library(rgl)
 library(readxl)
 library(nlme)
 library(mgcv)
-library("corrplot")
+library(corrplot)
+library(scatterplot3d)
 
-#Càrrega del dataset de les dades del pis de referencia - Parellada (A)
+# Load the dataset of the reference home
 Parellada <- read_excel("CASA_PARELLADAS.xlsx")
 datos_nci <- Parellada[2:19]
 
-#Escalat de dades de la matriu per a plotejar correctament el conjunt
+# Scale dataset in order to plot properly
 scaled_data <- scale(datos_nci)
 
-#Generació del model PCA per plotejat 3D
+# Create the PCA model
 rotated_data <- prcomp(scaled_data, center=TRUE, scale.=TRUE)$x
 pca3d <- PCA(rotated_data,ncp=3,graph=FALSE)
 
-# Set coordinates into auxiliar variables
+# Get some useful data from the PCA model
+PCA_center <- colMeans(pca3d$ind$coord)
+PCA_cov <- cov(pca3d$ind$coord)
+
+# Create an ellipsoid object to represent the conficende region
+ellipsoid <- ellipse3d(PCA_cov, centre = PCA_center)
+
+# Set coordinates of the PCA Analysis into auxiliary variables
 PCA_x <- pca3d$ind$coord[,1]
 PCA_y <- pca3d$ind$coord[,2]
 PCA_z <- pca3d$ind$coord[,3]
 
-#Plotejat 3D del model PCA
+# Plot the PCA model
 scatter3d(PCA_x, PCA_y, PCA_z, point.col = "steelblue",
           sphere.size = 1.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", surface = FALSE, ellipsoid = TRUE)
 
-# plot3d(PCA_x, PCA_y, PCA_z, col = "steelblue", 
-        # type = "s", size = 0.5, xlab = "PC1", ylab = "PC2", zlab = "PC3")
-
-#Carreguem les dades dels altres pisos
+# Load the datasets of the others homes
 Pompeu<- read_excel(path = "CASA_PADRE_MARC.xlsx")
 datos_Pompeu<- Pompeu[,2:19]
+
+# Scale dataset in order to plot properly
 scaled_Pompeu <- scale(datos_Pompeu)
+
+# Make a prediction based on the PCA model
 rotated_Pompeu <- prcomp(scaled_Pompeu, center=TRUE, scale.=TRUE)$x
 Pompeu_coords <- predict(pca3d, rotated_Pompeu)
 
-# Set coordinates into auxiliar variables
+# Set coordinates of the prediction into auxiliar variables
 Pompeu_x <- Pompeu_coords$coord[, 1]
 Pompeu_y <- Pompeu_coords$coord[, 2]
 Pompeu_z <- Pompeu_coords$coord[, 3]
 
-# Plot values
-scatter3d(PCA_x, PCA_y, PCA_z, point.col = "steelblue",
-          sphere.size = 1.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", surface = FALSE, ellipsoid = TRUE)
+# Plot PCA Analysis data
+plot3d(PCA_x, PCA_y, PCA_z, col = "purple", 
+       type = "s", size = 0.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", main="3D Scatter Plot")
 
-points3d(Pompeu_x, Pompeu_y, Pompeu_z, col = "red", pch = 19, cex = 1.5, xlab = "Eje X", zlab = "Eje Z")
+# Plot Prediction Model data
+plot3d(Pompeu_x, Pompeu_y, Pompeu_z, col = "yellow", 
+       type = "s", size = 0.5, add=TRUE)
 
-points3d(Pompeu_x, Pompeu_y, Pompeu_z, col = "red", 
-       type = "s", size = 0.5, xlab = "PC1", ylab = "PC2", zlab = "PC3", add=TRUE)
+# Plot the condicende region
+shade3d(ellipsoid, col = "blue", alpha = 0.5)
 
 # Add legend
 #legend3d("topright", legend = c("PCA Analysis", "Predicted"), col = c("blue", "green"), pch = 16)
 
-#Generació del model PCA en 2D i plotejat de gràfics representatius
+# Generació del model PCA en 2D i plotejat de gràfics representatius
 pca2d <- PCA(datos_nci,ncp=5,graph=FALSE)
 fviz_pca_var(pca2d, col.var = "contrib") # Scree plot
 fviz_pca_biplot(pca2d, col.var = "contrib") # Biplot
 
-#Dades i gràfics interessants sobre la variancia i desviació estàndard de les diferents variables
+# Dades i gràfics interessants sobre la variancia i desviació estàndard de les diferents variables
 summary(pca3d)
 eig.val <- get_eigenvalue(pca3d)
 fviz_eig(pca3d, addlabels = TRUE, ylim = c(0, 20))
