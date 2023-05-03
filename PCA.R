69f8e57a73cf749fc2ce2a4ceeef6af8bb9eb0d3
Zepolkram01 <marcelamo13250901@gmail.com>
2023-05-03 20:01:05 +0200

    new version that works properly

--- PCA.R
+++ PCA.R
@@ -30,15 +30,15 @@ datos_nci <- Parellada[2:17]
 scaled_data <- scale(datos_nci)
 
 # Create the PCA model
-rotated_data <- prcomp(scaled_data, center=TRUE, scale.=TRUE)$x
-pca3d <- PCA(rotated_data,ncp=3,graph=FALSE)
+#rotated_data <- prcomp(scaled_data, center=TRUE, scale=TRUE)#
+pca3d <- PCA(scaled_data,ncp=3,graph=TRUE)
 
 # Get some useful data from the PCA model
 PCA_center <- colMeans(pca3d$ind$coord)
 PCA_cov <- cov(pca3d$ind$coord)
 
 # Create an ellipsoid object to represent the conficende region
-ellipsoid <- ellipse3d(PCA_cov, centre = PCA_center,level = 0.95)
+ellipsoid <- ellipse3d(PCA_cov, centre = PCA_center,level = 0.9)
 
 # Set coordinates of the PCA Analysis into auxiliary variables
 PCA_x <- pca3d$ind$coord[,1]
@@ -85,29 +85,21 @@ scaled_Manso <- scale(datos_Manso)
 scaled_Hospi <- scale(datos_Hospi)
 
 # Make a prediction based on the PCA model
-rotated_Pompeu <- prcomp(scaled_Pompeu, center=TRUE, scale.=TRUE)$x
-Pompeu_coords <- predict(pca3d, rotated_Pompeu)
+Pompeu_coords <- predict(pca3d, scaled_Pompeu)
 
-rotated_Montigala <- prcomp(scaled_Montigala, center=TRUE, scale.=TRUE)$x
-Montigala_coords <- predict(pca3d, rotated_Montigala)
+Montigala_coords <- predict(pca3d, scaled_Montigala)
 
-rotated_Nil <- prcomp(scaled_Nil, center=TRUE, scale.=TRUE)$x
-Nil_coords <- predict(pca3d, rotated_Nil)
+Nil_coords <- predict(pca3d, scaled_Nil)
 
-rotated_Pitagoras <- prcomp(scaled_Pitagoras, center=TRUE, scale.=TRUE)$x
-Pitagoras_coords <- predict(pca3d, rotated_Pitagoras)
+Pitagoras_coords <- predict(pca3d, scaled_Pitagoras)
 
-rotated_Junta <- prcomp(scaled_Junta, center=TRUE, scale.=TRUE)$x
-Junta_coords <- predict(pca3d, rotated_Junta)
+Junta_coords <- predict(pca3d, scaled_Junta)
 
-rotated_Erasme <- prcomp(scaled_Erasme, center=TRUE, scale.=TRUE)$x
-Erasme_coords <- predict(pca3d, rotated_Erasme)
+Erasme_coords <- predict(pca3d, scaled_Erasme)
 
-rotated_Manso <- prcomp(scaled_Manso, center=TRUE, scale.=TRUE)$x
-Manso_coords <- predict(pca3d, rotated_Manso)
+Manso_coords <- predict(pca3d, scaled_Manso)
 
-rotated_Hospi <- prcomp(scaled_Hospi, center=TRUE, scale.=TRUE)$x
-Hospi_coords <- predict(pca3d, rotated_Hospi)
+Hospi_coords <- predict(pca3d, scaled_Hospi)
 
 # Set coordinates of the prediction into auxiliar variables
 Pompeu_x <- Pompeu_coords$coord[, 1]
@@ -150,15 +142,6 @@ plot3d(PCA_x, PCA_y, PCA_z, col = "purple",
 plot3d(Pompeu_x, Pompeu_y, Pompeu_z, col = "yellow", 
        type = "s", size = 0.5, add=TRUE)
 
-plot3d(Montigala_x, Montigala_y, Montigala_z, col = "red", 
-       type = "s", size = 0.5, add=TRUE)
-
-plot3d(Nil_x, Nil_y, Nil_z, col = "orange", 
-       type = "s", size = 0.5, add=TRUE)
-
-plot3d(Pitagoras_x, Pitagoras_y, Pitagoras_z, col = "green", 
-       type = "s", size = 0.5, add=TRUE)
-
 plot3d(Junta_x, Junta_y, Junta_z, col = "blue", 
        type = "s", size = 0.5, add=TRUE)
 
@@ -168,31 +151,44 @@ plot3d(Erasme_x, Erasme_y, Erasme_z, col = "black",
 plot3d(Manso_x, Manso_y, Manso_z, col = "pink", 
        type = "s", size = 0.5, add=TRUE)
 
+plot3d(Montigala_x, Montigala_y, Montigala_z, col = "red", 
+       type = "s", size = 0.5, add=TRUE)
+
+plot3d(Nil_x, Nil_y, Nil_z, col = "orange", 
+       type = "s", size = 0.5, add=TRUE)
+
+plot3d(Pitagoras_x, Pitagoras_y, Pitagoras_z, col = "green", 
+       type = "s", size = 0.5, add=TRUE)
+
 plot3d(Hospi_x, Hospi_y, Hospi_z, col = "brown", 
        type = "s", size = 0.5, add=TRUE)
 
 # Plot the condicende region
 shade3d(ellipsoid, col = "blue", alpha = 0.5)
 
-# Add legend
-#legend3d("topright", legend = c("PCA Analysis", "Predicted"), col = c("blue", "green"), pch = 16)
+#Add legend
+legend3d("right", legend = c("Parellada", "Pompeu","Junta","Erasme","Manso","Montigalà","Nil","Pitàgores","Hospi"), col = c("purple", "yellow","blue","black","pink","red","orange","green","brown"), pch = 16)
 
 # Generació del model PCA en 2D i plotejat de gràfics representatius
-pca2d <- PCA(datos_nci,ncp=5,graph=FALSE)
+pca2d <- PCA(scaled_data,ncp=5,graph=FALSE)
 fviz_pca_var(pca2d, col.var = "contrib") # Scree plot
 fviz_pca_biplot(pca2d, col.var = "contrib") # Biplot
+cor_matrix <- cor(scaled_data)
+corrplot(cor_matrix, method = "color")
 
 # Dades i gràfics interessants sobre la variancia i desviació estàndard de les diferents variables
 summary(pca3d)
+
 eig.val <- get_eigenvalue(pca3d)
-fviz_eig(pca3d, addlabels = TRUE, ylim = c(0, 20))
+fviz_eig(pca3d, addlabels = TRUE, ylim = c(0, 50))
 
 var <- get_pca_var(pca3d)
 corrplot(var$contrib, is.corr=FALSE)
+plot3d(cor_matrix, type = "s", size = 2)
 
 # Contributions of variables to PC1
 fviz_contrib(pca3d, choice = "var", axes = 1, top = 10)
 
-cor_matrix <- cor(datos_nci)
-plot3d(cor_matrix, type = "s", size = 2)
+
+
 
